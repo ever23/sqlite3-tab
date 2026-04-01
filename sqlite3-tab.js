975cda135f02086d20c1e9f0cf0600ad93955d18
Enyerber Franco <enyerverfranco@gmail.com>
2026-04-01 13:21:12 -0400

    removido console.log de depuración

--- sqlite3-tab.js
+++ sqlite3-tab.js
@@ -1,37 +1,32 @@
-const sqlite3=require("sqlite3").verbose()
-const sqlite3Tabla=require("./lib/sqlite3Tabla")
-const {SQLITE3_DB,connect}= sqlite3Tabla
-const model=require("tabla-model")
+const sqlite3 = require("sqlite3").verbose()
+const sqlite3Tabla = require("./lib/sqlite3Tabla")
+const { SQLITE3_DB, connect } = sqlite3Tabla
+const model = require("tabla-model")
 //console.log(sqlite3)
 /**
 * mysqlTable
 * crea una conexion a una base de datos mysql
 */
-class sqlite3Tab extends connect
-{
+class sqlite3Tab extends connect {
     /**
     * @param {object} param - configuracion para sqlite3 o objeto Database de sqlite3
     *
     */
-    constructor(params)
-    {
-        if(params instanceof sqlite3.Database)
-        {
-            super({},SQLITE3_DB)
-            this.connection=params
-        }else {
-            super(params,SQLITE3_DB)
-            this.connection=new sqlite3.Database(params)
+    constructor(params) {
+        if (params instanceof sqlite3.Database) {
+            super({}, SQLITE3_DB)
+            this.connection = params
+        } else {
+            super(params, SQLITE3_DB)
+            this.connection = new sqlite3.Database(params)
         }
-        this._escapeChar="`"
+        this._escapeChar = "`"
         this._information_schema = "select sqlite_master.* from sqlite_master where  name="
     }
-    serialize(call)
-    {
+    serialize(call) {
         return this.connection.serialize(call)
     }
-    parallelize(call)
-    {
+    parallelize(call) {
         return this.connectionparallelize(call)
     }
     /**
@@ -41,30 +36,26 @@ class sqlite3Tab extends connect
     * @param {function} callback - funcion anomina que se ejecutara cuando se verifique la existencia de la tabla
     * @return {dbTabla}
     */
-    tabla(tabla,callback,verify=false)
-    {
-        if(typeof callback ==="boolean")
-            verify=callback
-        console.log('DEBUG: this._caheTablas is:', this._caheTablas);
-        if(typeof this._caheTablas[tabla]!=="undefined")
-        {
-            typeof callback==="function"?callback(this._caheTablas[tabla]):null
+    tabla(tabla, callback, verify = false) {
+        if (typeof callback === "boolean")
+            verify = callback
+        if (typeof this._caheTablas[tabla] !== "undefined") {
+            typeof callback === "function" ? callback(this._caheTablas[tabla]) : null
             return this._caheTablas[tabla]
         }
-        return  this._caheTablas[tabla] = new sqlite3Tabla({
-            tabla:tabla,
-            connection:this,
-            callback:t=>typeof callback==="function"?callback(t):null,
-            config:this.helpersConf()
-        },typeof callback==="function" && verify)
+        return this._caheTablas[tabla] = new sqlite3Tabla({
+            tabla: tabla,
+            connection: this,
+            callback: t => typeof callback === "function" ? callback(t) : null,
+            config: this.helpersConf()
+        }, typeof callback === "function" && verify)
 
     }
     /**
     * connecta con la base de datos
     *
     */
-    connect()
-    {
+    connect() {
         //this.connection.connect(callback)
     }
     /**
@@ -72,26 +63,19 @@ class sqlite3Tab extends connect
     * @param {string} query - consulta sql
     * @return {Promise}
     */
-    query(query,...params)
-    {
-        return new Promise((resolver,reject)=>
-        {
+    query(query, ...params) {
+        return new Promise((resolver, reject) => {
 
-            if(/^[\s]*select/i.test(query))
-            {
-                this.connection.all(query,...params,(error,result)=>
-                {
-                    if(error)
-                    {
+            if (/^[\s]*select/i.test(query)) {
+                this.connection.all(query, ...params, (error, result) => {
+                    if (error) {
                         return reject(error)
                     }
                     resolver(result)
                 })
-            }else{
-                this.connection.run(query,...params,(error,result)=>
-                {
-                    if(error)
-                    {
+            } else {
+                this.connection.run(query, ...params, (error, result) => {
+                    if (error) {
                         return reject(error)
                     }
                     resolver(result)
@@ -105,14 +89,10 @@ class sqlite3Tab extends connect
     * @param {string} query - consulta sql
     * @return {Promise}
     */
-    get(query,param={})
-    {
-        return new Promise((resolver,reject)=>
-        {
-            this.connection.get(query,param,(error,result)=>
-            {
-                if(error)
-                {
+    get(query, param = {}) {
+        return new Promise((resolver, reject) => {
+            this.connection.get(query, param, (error, result) => {
+                if (error) {
                     return reject(error)
                 }
                 resolver(result)
@@ -124,23 +104,18 @@ class sqlite3Tab extends connect
     * @param {string} query - consulta sql
     * @return {Promise}
     */
-    each(query,param={},callback)
-    {
-        return new Promise((resolver,reject)=>
-        {
-            if(typeof param==="function")
-            {
-                callback=param
-                param={}
+    each(query, param = {}, callback) {
+        return new Promise((resolver, reject) => {
+            if (typeof param === "function") {
+                callback = param
+                param = {}
             }
-            this.connection.each(query,param,(error,result)=>
-            {
-                if(error)
-                {
+            this.connection.each(query, param, (error, result) => {
+                if (error) {
                     return reject(error)
                 }
                 callback(result)
-            },resolver)
+            }, resolver)
         })
     }
 
@@ -148,15 +123,13 @@ class sqlite3Tab extends connect
     /**
     * termina la conexion
     */
-    end()
-    {
+    end() {
         this.connection.close()
     }
     /**
     * termina la conexion
     */
-    close()
-    {
+    close() {
         this.connection.close()
     }
     /**
@@ -165,23 +138,18 @@ class sqlite3Tab extends connect
     * el segundo parametro
     * @param {string} table - nombre de la tabla
     */
-    _keysInTable(table)
-    {
-        return new Promise((res,rej)=>
-        {
+    _keysInTable(table) {
+        return new Promise((res, rej) => {
             this.query(`${this._information_schema}'${table}'`)
-                .then(result=>{
-                    this.inModel(table,result.length==0)
-                        .then(res).catch(e=>
-                        {
-                            if(e===undefined)
-                            {
-                                if(result.length==0)
+                .then(result => {
+                    this.inModel(table, result.length == 0)
+                        .then(res).catch(e => {
+                            if (e === undefined) {
+                                if (result.length == 0)
                                     rej("la tabla no existe")
                                 else
-                                    res((new model(result[0].sql,this._escapeChar)).getData())
-                            }else
-                            {
+                                    res((new model(result[0].sql, this._escapeChar)).getData())
+                            } else {
                                 rej(e)
                             }
                         })
@@ -192,4 +160,4 @@ class sqlite3Tab extends connect
 
 }
 
-module.exports=sqlite3Tab
+module.exports = sqlite3Tab
